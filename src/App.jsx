fce4f06ffc2c9f2015ada9f204c993f82248f4e9
EssiJunior <nessipjunior@gmail.com>
2025-08-30 07:03:11 +0100

    Layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // REACT IMPORTS
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+// LAYOUT
+import Layout from './Layouts'
+
 // PAGES
 import Home from './pages/Home'
 import Product from './pages/Product'
@@ -9,8 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/product" element={<Product />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
